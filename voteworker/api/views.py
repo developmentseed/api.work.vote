e9c38a7c400f846e3378e2d522701a90e536cc4c
Scisco <scisco7@gmail.com>
2015-12-21 05:31:34 -0500

    add intersects filter

--- voteworker/api/views.py
+++ voteworker/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import permissions
-
+from django.contrib.gis.geos import Point
 
 from .serializer import StateSerializer, JurisdictionSerializer
 from jurisdiction.models import State, Jurisdiction
@@ -16,3 +16,18 @@ class JurisdictionViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Jurisdiction.objects.all()
     serializer_class = JurisdictionSerializer
+
+    # Build the queryset based on params
+    def get_queryset(self):
+        request = self.request
+
+        # Return latest processed images first
+        queryset = self.queryset.extra(order_by=['name'])
+
+        if 'contains' in request.GET:
+            coords = request.GET.get('contains').split(',')
+            coords = map(float, coords)
+            pnt = Point(coords[1], coords[0])
+            queryset = queryset.filter(geometry__contains=pnt)
+
+        return queryset
