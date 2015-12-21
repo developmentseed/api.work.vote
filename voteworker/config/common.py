6e5781ba9505c024ce99a7a5599d572f6dca7a01
Scisco <scisco7@gmail.com>
2015-12-21 05:40:27 -0500

    add pagination

--- voteworker/config/common.py
+++ voteworker/config/common.py
@@ -256,6 +256,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
 }
 # END REST FRAMEWORK CONFIGURATION
 
