ae8adc4f0b7e572e475c537b04e1b1e6eb4efceb
Scisco <scisco7@gmail.com>
2016-01-06 09:46:11 -0500

    add geocode search

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,9 +1,31 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 from django.contrib.gis.geos import Point
+import geocoder
 
 from .serializer import StateSerializer, JurisdictionSerializer
 from jurisdiction.models import State, Jurisdiction
+
+
+def geocode(address, required_precision_km=1.):
+    """ Identifies the coordinates of an address
+    :param address:
+        the address to be geocoded
+    :type value:
+        String
+    :param required_precision_km:
+        the maximum permissible geographic uncertainty for the geocoding
+    :type required_precision_km:
+        float
+    :returns:
+        dict
+    :example:
+        >>> geocode('1600 Pennsylvania Ave NW, Washington, DC 20500')
+        {'lat': 38.89767579999999, 'lon': -77.0364827}
+    """
+    geocoded = geocoder.google(address)
+    (lat, lon) = geocoded.geometry['coordinates']
+    return [lat, lon]
 
 
 class StateViewSet(viewsets.ReadOnlyModelViewSet):
@@ -23,6 +45,19 @@ class JurisdictionViewSet(viewsets.ReadOnlyModelViewSet):
 
         # Return latest processed images first
         queryset = self.queryset.extra(order_by=['name'])
+
+        if 'search' in request.GET:
+            search = request.GET.get('search')
+            county = queryset.filter(name__startswith=search)
+
+            if not county:
+                coords = geocode(search)
+
+                if coords:
+                    pnt = Point(*coords)
+                    queryset = queryset.filter(geometry__contains=pnt)
+            else:
+                queryset = county
 
         if 'contains' in request.GET:
             coords = request.GET.get('contains').split(',')
