3856801dfe11bc6429f93cb131b36efa4c1a600c
Scisco <scisco7@gmail.com>
2016-01-13 01:03:59 -0500

    add field for voter registration

--- apps/jurisdiction/models.py
+++ apps/jurisdiction/models.py
@@ -31,6 +31,8 @@ class Jurisdiction(models.Model):
                                            '- Input: S or J', null=True, blank=True)
     pre_registration = models.TextField('If pre-registration is available, is it required for 16/17 yo\'s?' +
                                         ' - Input: Y or N', null=True, blank=True)
+    voter_registration_url = models.CharField('Website for online voter regisration', max_length=300,
+                                              blank=True, null=True)
     minimum_age = models.CharField('Minimum age - Input: ##', max_length=250, null=True, blank=True)
     high_school_student = models.TextField('Can high school students work? - Input: Y or N', null=True, blank=True)
     hours_start = models.CharField('Hours start', max_length=250, null=True, blank=True)
