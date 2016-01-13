10d90c710917c15f9b3467b7456a96946b2487ef
Scisco <scisco7@gmail.com>
2016-01-13 15:18:39 -0500

    user html render for web requests

--- config/settings/common.py
+++ config/settings/common.py
@@ -252,6 +252,7 @@ LOGGING = {
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer'
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
