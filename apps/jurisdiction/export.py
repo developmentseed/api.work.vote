320c5078faa4ec5edb84a7cd1b7edb13fcd749e0
Scisco <scisco7@gmail.com>
2016-03-24 15:33:43 -0400

    export function for apps

---
+++ apps/jurisdiction/export.py
@@ -0,0 +1,35 @@
+import csv
+from django.http import HttpResponse
+from django.utils.encoding import smart_str
+
+from jurisdiction.models import Jurisdiction
+
+
+def export_jurisdiction_emails():
+
+    jurisdictions = Jurisdiction.objects.filter(email__isnull=False)
+
+    r = HttpResponse(content_type='text/csv')
+    r['Content-Disposition'] = 'attachment; filename=jurisdiction_emails.csv'
+    writer = csv.writer(r, csv.excel)
+    r.write(u'\ufeff'.encode('utf8'))  # BOM (optional...Excel needs it to open UTF-8 file properly)
+
+    headings = [
+        smart_str(u"jurisdiction_id"),
+        smart_str(u"name"),
+        smart_str(u"state"),
+        smart_str(u"email")
+    ]
+
+    writer.writerow(headings)
+
+    for j in jurisdictions:
+        row = [
+            smart_str(j.id),
+            smart_str(j.name),
+            smart_str(j.state.name),
+            smart_str(j.email)
+        ]
+        writer.writerow(row)
+
+    return r
