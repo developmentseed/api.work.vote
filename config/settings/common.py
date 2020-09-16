467b41866b96be8661c74e53cd6219059fc69fe4
Magneton <mlong@magneton.io>
2020-09-16 10:52:34 -0500

    update packages

--- config/settings/common.py
+++ config/settings/common.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 # ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
